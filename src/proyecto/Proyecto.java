5a2f9e5b2acaba620bfa4182878c4f0fb421350f
rikumax25 <jorgev259@outlook.es>
2018-03-26 20:25:21 -0600

    Commit de prueba

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -16,6 +16,8 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //comentario de demostracion
     }
     
 }
